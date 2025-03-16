9b38973d17f56c012f9135afc5d8c72262882a1c
Jan Kaczorowski <jan.kaczorowski@gmail.com>
2025-03-16 08:13:37 +0100

    fix

--- app/services/class_booker.rb
+++ app/services/class_booker.rb
@@ -10,7 +10,7 @@ class ClassBooker
 
   def call
     zdrofit_api_client = user.zdrofit_api_client
-    zdrofit_api_client.book_class(booking.class_id)
+    zdrofit_api_client.book_class(class_id: booking.class_id, club_id: booking.club_id)
     booking.update!(status: "booked", next_occurrence: booking.next_occurrence + 1.week)
     ClassBookerJob.set(wait_until: booking.booking_time).perform_later(booking.id)
   rescue => e
