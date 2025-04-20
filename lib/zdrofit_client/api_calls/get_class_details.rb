81b6d246e4d7c4c99ba956f4ca2b810469509bf7
Jan Kaczorowski <jan.kaczorowski@gmail.com>
2025-04-20 10:25:38 +0200

    cleanup

--- lib/zdrofit_client/api_calls/get_class_details.rb
+++
@@ -1,9 +0,0 @@
-module ZdrofitClient
-  module ApiCalls
-    class GetClassDetails < ApiCall
-      def call(class_id:)
-        @client.get("/Classes/ClassCalendar/Details?classId=#{class_id}")
-      end
-    end
-  end
-end 
